da30ca61f9d3ae4dfa0b4019c10969659c85ad46
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2023-12-14 07:59:43 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,27 @@
+
+import random
+
+
+    main()
+def main():
+    return data
+    for item in data:
+def generate_random_data():
+
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()    main()
+    return data
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    for item in data:
+import random
+
+
+
+def main():
+    data = generate_random_data()
+def generate_random_data():
+        print(f"Random Number: {item}")
