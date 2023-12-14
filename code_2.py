4c58971fb71bf01988b4b79ff94fea066d123314
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2023-12-14 13:02:07 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,53 +1,27 @@
-
-    main()
-def generate_random_data():
-    data = generate_random_data()
-
-
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
-    return data
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-def main():
     for item in data:
 
-
-def main():
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-    data = generate_random_data()
-import random
-
-    return data
 if __name__ == "__main__":
 def generate_random_data():
-
-        print(f"Random Number: {item}")def generate_random_data():
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-
-
-    main()
-        print(f"Random Number: {item}")
-
-def main():
-    data = generate_random_data()
-import random
     return data
-    for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+        print(f"Random Number: {item}")
     main()
 
 import random
-
-if __name__ == "__main__":
     data = generate_random_data()
+    main()
 
+    return data
+    data = generate_random_data()
 def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
-        print(f"Random Number: {item}")
     for item in data:
 
 def main():
+
+
+if __name__ == "__main__":
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
